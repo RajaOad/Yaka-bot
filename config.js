47ad8a27d5ff0d5fecc85b9b3b1d0b0ef65c52ff
RajaOad <130591119+RajaOad@users.noreply.github.com>
2024-06-24 10:02:13 +0500

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ mongoose.set('strictQuery', true);
 
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "94774516277";   // You can Change this number //
+  gg = "923063080758";   // You can Change this number //
 }
 
 // -------------------------------------------------------------- //
